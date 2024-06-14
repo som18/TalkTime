fcb11c8eaf69f8501d0da86f0acb8069a141dfcc
som18 <93382740+som18@users.noreply.github.com>
2024-06-14 18:27:07 +0530

    deploy the full flow with socketio server

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-const app = express();
-const PORT = process.env.PORT || 5000;
+import { app, server } from "./socket/socket.js";
 
 
 dotenv.config();
+// const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,7 +25,7 @@ app.use("/api/users", userRoutes);
 // })
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
